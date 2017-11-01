494184555e7fb6fbe60b88da9722989f5d446f07
Vaisakh Pradeep <vaisakh.p@onnurinet.com>
2017-11-01 18:22:06 +0530

    site is fully functional and responsive

--- js/main.js
+++ js/main.js
@@ -15,6 +15,22 @@ $(document).on('click', 'a', function(event){
     }, 500);
 });
 
+$(document).ready(function(){
+
+	$(window).scroll(function(){
+		if($(this).scrollTop()>100){
+			$(".scrollup").fadeIn();
+		} else{
+			$(".scrollup").fadeOut();
+		}
+	});
+	$(".scrollup").click(function(){
+		$("html,body").animate({scrollTop:0}, 600);
+		return false;
+	});
+
+});
+
 /*$(document).ready(function(){
 
 
