53cfe97110bdd6bb8b35006a84c2b48ae6c64dc4
Vaisakh Pradeep <vaisakh.p@onnurinet.com>
2017-11-01 17:29:38 +0530

    scroll nav added

--- js/main.js
+++ js/main.js
@@ -6,3 +6,31 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+});
+
+/*$(document).ready(function(){
+
+
+
+  $('a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 1000);
+       return false;
+      }
+    }
+  });
+});*/
